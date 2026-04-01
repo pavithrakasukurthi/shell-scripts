4ddadfa5ee6cbb6a2a764e1d4fe2bec0505fc313
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-01 13:57:15 +0530

    mysql installation script 1

--- mysql-install.sh
+++ mysql-install.sh
@@ -4,13 +4,15 @@ R="\033[31m"
 G="\033[32m"
 Y="\033[33m"
 
-if [ $($(id -u)) -ne 0 ]; then
+if [ $(id -u) -ne 0 ]; then
     echo -e "$R Execute this script with root previlages $R"
+    exit 1
 fi
 
 dnf list installed mysql
 if [ $? -eq 0 ]; then
     echo -e "$Y mysql already installed...SKIPPING INSTALLATION $Y"
+    exit 1
 else
     dnf install mysql -y
     echo -e "$G INSTALLATION SUCESS"
