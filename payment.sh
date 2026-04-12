a8ad44217759786ec42b4ae85453c7c4adcbf93d
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-12 23:41:10 +0530

    sunday 20

--- payment.sh
+++ payment.sh
@@ -4,13 +4,13 @@ source ./common.sh
 
 user_auth 
 
-dnf install python3 gcc python3-devel -y
+dnf install python3 gcc python3-devel -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
 sys_user_check
 
 app_configuration payment
 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "Installed dependencies"
 
 service_file_check payment
