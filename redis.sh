638ca0ad5f4eeff0743d3fcf6c22834e34248349
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-12 22:54:34 +0530

    sunday 12

--- redis.sh
+++ redis.sh
@@ -17,7 +17,7 @@ if [ "$CURR_IP" == "0.0.0.0" ]; then
 else
     echo "taking backup of conf file"
     cp -p /etc/redis/redis.conf /etc/redis/redis.conf_$(date +%Y-%m-%d)
-    sed -i 's/bindIp:.*/bindIp: 0.0.0.0/' /etc/mongod.conf
+    sed -i 's/bindIp:.*/bindIp: 0.0.0.0/' /etc/redis/redis.conf
     sed -i 's/protected-mode yes/protected-mode no/' /etc/redis/redis.conf
     echo "updated bindIp to 0.0.0.0 and protected-mode restarting the service"
     systemctl restart redis
