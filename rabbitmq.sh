1e2d299f058c5df764efac06543eb5a80b4a9ea3
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-12 23:30:09 +0530

    sunday 19

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,13 +6,15 @@ user_auth
 
 create_repo rabbitmq
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "rabbitmq installed"
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
-systemctl status rabbitmq-server
+systemctl status rabbitmq-server &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "Rabbitmq is up and running"
 
+echo "creating rabbit user unlike app user..."
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+validate $? "Permissions have been set"
--- shipping.sh
+++ shipping.sh
@@ -4,27 +4,27 @@ source ./common.sh
 
 user_auth
 
-dnf install maven -y
+dnf install maven -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
 sys_user_check
 
 app_configuration shipping
 
-mvn clean package 
+mvn clean package &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 mv target/shipping-1.0.jar shipping.jar 
 validate $? "Renamed artifact"
 
 service_file_check shipping
 
 
-dnf install mysql -y 
+dnf install mysql -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "mysql installed"
 
-mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/schema.sql &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
-mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/app-user.sql &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
-mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.pavithra.sbs -uroot -pRoboShop@1 < /app/db/master-data.sql &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
 systemctl restart shipping
 validate $? "shipping service has been restarted"
