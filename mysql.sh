2c657c74140caff03e18ffe0aab61d492c8af5f5
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-12 23:16:35 +0530

    sunday 16

--- mysql.sh
+++ mysql.sh
@@ -4,16 +4,17 @@ source ./common.sh
 
 user_auth
 
-dnf list installed mysql-server
+dnf list installed mysql-server &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 if [ $? -eq 0 ]; then
     echo "mysql already installed"
 else
-    dnf install mysql-server -y
+    dnf install mysql-server -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
     validate $? "mysql-server installed"
 fi
 
 systemctl enable mysqld
 systemctl start mysqld  
+systemct status mysqld &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "mysql is up and running"
 
 echo "setting up root password for mysql"
