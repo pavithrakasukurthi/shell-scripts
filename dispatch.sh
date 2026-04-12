b2cc90b9bd13bc4531df08129afb6e4954ff1ad5
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-12 23:41:50 +0530

    sunday 21

--- dispatch.sh
+++ dispatch.sh
@@ -4,15 +4,15 @@ source ./common.sh
 
 user_auth
 
-dnf install golang -y
+dnf install golang -y &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 validate $? "installed golang"
 
 sys_user_check
 
 app_configuration dispatch
 
-go mod init dispatch
-go get 
-go build
+go mod init dispatch &> /var/log/catalogue.log_$(date +%Y-%m-%d)
+go get &> /var/log/catalogue.log_$(date +%Y-%m-%d)
+go build &> /var/log/catalogue.log_$(date +%Y-%m-%d)
 
 service_file_check dispatch
